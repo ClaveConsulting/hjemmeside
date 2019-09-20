e96097c0892e2281a2ac44052a44438d180926a1
Morten S. Tangen <morten.tangen@eika.no>
2019-09-20 13:23:47 +0200

    endre tekst

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hjemmeside under construction</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Velkommen til vår hjemmeside</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
