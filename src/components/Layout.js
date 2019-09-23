cae9bf291276daa70c2e236bb008c057f9de5893
Morten S. Tangen <morten.tangen@eika.no>
2019-09-23 16:02:30 +0200

    feat: bruk clave bilde. Inkluder ny paragraph nedenfor i hudfarge

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,22 +9,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import Header from './Header'
 import './layout.css'
 
 const Layout = ({ children }) => {
   return (
     <Wrapper>
-      <Header />
       <main>{children}</main>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  margin: 0 auto;
   max-width: 1366px;
-  padding: 0 1.45rem 1.0875rem 1.45rem;
 `
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
-
-import Layout from '../components/Layout'
 import Image from '../components/Image'
 import SEO from '../components/Seo'
 import { COLOR_CLAVE_GREEN, COLOR_CLAVE_SKIN } from '../colors'
 import ClaveLink from '../components/ClaveLink'
+import Header from '../components/Header'
+import Title from '../components/Title'
+import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Forside" />
-    <BackgroundImage>
-      <Image />
-    </BackgroundImage>
-    <Overlay />
-    <TextPanel>
-      <Title>Vi brenner for de beste løsningene.</Title>
-      <ClaveLink to="/page-2/">Bli bedre kjent med oss</ClaveLink>
-    </TextPanel>
+    <div style={{ height: '100vh' }}>
+      <Header />
+      <StyledBackgroundImage>
+        <Image />
+      </StyledBackgroundImage>
+      <StyledOverlay />
+      <StyledTextPanel>
+        <Title color={COLOR_CLAVE_SKIN} bla="noe">Vi brenner for de beste løsningene.</Title>
+        <ClaveLink to="/page-2/">Bli bedre kjent med oss</ClaveLink>
+      </StyledTextPanel>
+    </div>
+    <div style={{ height: '100vh', width: '100vw', backgroundColor: COLOR_CLAVE_SKIN }}>
+      <StyledFlexWrapper>
+        <StyledTextPanel>
+          <Title color={COLOR_CLAVE_GREEN}>Her står det forklarende hva Clave gjør</Title>
+        </StyledTextPanel>
+      </StyledFlexWrapper>
+    </div>
   </Layout>
 )
 
-const TextPanel = styled.div`
+const StyledTextPanel = styled.div`
   padding-top: 10rem;
 `
 
-const BackgroundImage = styled.div`
+const StyledFlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const StyledBackgroundImage = styled.div`
   position: absolute;
   top: 0;
   left: 0;
@@ -36,9 +52,10 @@ const BackgroundImage = styled.div`
   background-color: ${COLOR_CLAVE_SKIN};
 `
 
-const Overlay = styled.div`
+const StyledOverlay = styled.div`
   position: absolute;
   overflow-x: hidden;
+  overflow-y: hidden;
   top: 0;
   left: 0;
   height: 100vh;
@@ -56,7 +73,4 @@ const Overlay = styled.div`
   }
 `
 
-const Title = styled.h1`
-  color: ${COLOR_CLAVE_SKIN};
-`
 export default IndexPage
