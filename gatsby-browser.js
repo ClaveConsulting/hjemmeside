e9f236da609ecc23dd29e4970442ce77c27d404e
Morten S. Tangen <morten.tangen@eika.no>
2019-09-30 08:11:09 +0200

    feat: remove provider for media queries

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-import React from 'react';
-import WindowDimensionsProvider from './src/components/WindowDimensionProvider';
-
-export const wrapRootElement = ({ element }) =>
-  <WindowDimensionsProvider>{element}</WindowDimensionsProvider>;
