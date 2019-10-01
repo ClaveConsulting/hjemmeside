941d6c167dd81a4b36e861d6e0dd8706d9f1c48e
Morten S. Tangen <morten.tangen@eika.no>
2019-10-01 20:09:09 +0200

    chore: format

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import { COLOR_CLAVE_SKIN } from '../colors'
+import PropTypes from 'prop-types';
+import React from 'react';
+import { COLOR_CLAVE_SKIN } from '../colors';
 import styled from 'styled-components';
-import ClaveLink from './ClaveLink'
+import ClaveLink from './ClaveLink';
 
 const Header = () => (
   <header>
@@ -12,17 +12,17 @@ const Header = () => (
       <ClaveLink>Bli en av oss</ClaveLink>
     </Wrapper>
   </header>
-)
+);
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 1.45rem 1.0875rem;
   color: ${COLOR_CLAVE_SKIN};
-`
+`;
 
 Header.propTypes = {
   children: PropTypes.string,
-}
+};
 
-export default Header
+export default Header;
