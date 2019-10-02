070b9e049f2840ef993f2e460a2d87778b63dd05
Christian <christian766@gmail.com>
2019-10-02 18:12:07 +0200

    Added Clave icon to browser tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#005550`,
         theme_color: `#005550`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/clave-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
