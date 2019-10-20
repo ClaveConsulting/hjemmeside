a240e791b2cd1afaaba5cb67d0e48d21cf23024b
Yngve Svalestuen <yngve.svalestuen@clave.no>
2019-10-20 18:07:10 +0200

    Add light blue color for 'Bli en av oss' section background.

--- src/colors.js
+++ src/colors.js
@@ -2,3 +2,4 @@ export const COLOR_CLAVE_GREEN = '#005550';
 export const COLOR_CLAVE_SKIN = '#FFE6CD';
 export const COLOR_CLAVE_PINK = '#FA9691';
 export const COLOR_CLAVE_BLAA = '#002850';
+export const COLOR_CLAVE_LIGHTBLUE = '#BAD6E0';
