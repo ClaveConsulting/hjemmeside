8429c11669f3b6346bfd8bbff3988a477a5c1bc9
Eirik Vilpponen <eirik.vilpponen@nav.no>
2019-10-25 15:53:04 +0200

    bytte ut forsidebilde

--- src/components/Image.js
+++ src/components/Image.js
@@ -16,7 +16,7 @@ import Img from 'gatsby-image';
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "frontpage.jpeg" }) {
+      placeholderImage: file(relativePath: { eq: "frontpage.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1366) {
             ...GatsbyImageSharpFluid
