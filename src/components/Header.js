13319f6686a5ddba729da1c6b08be37d3e2165ee
Mathias Aarseth Pedersen <Mathiasaarseth.pedersen@naf.no>
2019-11-22 15:00:56 +0100

    Created a template component for project pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -123,6 +123,12 @@ const FrontPageLogo = styled(Logo)`
   fill: ${COLOR_CLAVE_SKIN};
 `;
 
+const HeaderLink = styled(ClaveLink)`
+  display: block;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+`;
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
