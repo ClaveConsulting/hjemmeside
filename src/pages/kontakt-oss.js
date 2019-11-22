14ba7ef67aeeb2a5d8129ca0433e9aed31a9c341
Christian <christian766@gmail.com>
2019-11-22 13:45:08 +0100

    Test with forms

--- src/pages/kontakt-oss.js
+++ src/pages/kontakt-oss.js
@@ -68,7 +68,7 @@ const SecondPage = () => {
                     <p>Trenger du hjelp til noe, ønsker å vite mer om hvordan det er å jobbe hos oss eller rett og slett
                         bare er litt nysgjerrig?</p>
                     <form name="message" method="POST" data-netlify="true">
-                        <CustomInput name="navn" placeholder="Navn" type="text"></CustomInput><br/>
+                        <CustomInput  placeholder="Navn" type="text"></CustomInput><br/>
                         <CustomInput name="Epost" placeholder="Epost" type="text"></CustomInput><br/>
                         <CustomInput name="Melding" placeholder="Melding" type="text"></CustomInput><br/>
                         <CustomSubmit value="Send" type="submit"></CustomSubmit>
