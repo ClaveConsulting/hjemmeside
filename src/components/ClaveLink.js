6014e16b2e0a47232899af61f40e23ca659d0b3b
Mathias Aarseth Pedersen <Mathiasaarseth.pedersen@naf.no>
2020-05-05 13:15:33 +0200

    Understrek på lenker skal ha en animasjon som endrer fargen på linjen

--- src/components/ClaveLink.js
+++ src/components/ClaveLink.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
-import { COLOR_CLAVE_PINK, COLOR_CLAVE_GREEN } from '../colors';
+import styled, { keyframes, css } from 'styled-components';
+import { COLOR_CLAVE_PINK, COLOR_CLAVE_GREEN, COLOR_CLAVE_YELLOW } from '../colors';
 import { ColorContext } from './Layout';
 import { onMobile } from './Breakpoints';
 
@@ -13,25 +13,70 @@ const ClaveLink = ({ to, children, className, ...props }) => {
   `;
 
   return (
-    <ColoredLink className={className} to={to} {...props}>
-      {children}
-    </ColoredLink>
+    <Underline>
+      <ColoredLink className={className} to={to} {...props}>
+        {children}
+      </ColoredLink>
+    </Underline>
   );
 };
+
+const hoverKeyframes = keyframes`
+0% {
+  background: ${COLOR_CLAVE_PINK};
+  transform: scaleX(1);
+}
+50% {
+  background: ${COLOR_CLAVE_PINK};
+  transform: scaleX(0.0); }
+
+51% {
+  background: ${COLOR_CLAVE_YELLOW};
+  transform: scaleX(0.0); }
+100% {
+  background: ${COLOR_CLAVE_YELLOW};
+  transform: scaleX(1.0);
+}
+`;
+
+const hoverAnimation = props => css`
+  ${hoverKeyframes} 0.5s;
+`;
+
+const Underline = styled.div`
+  display: flex;
+  flex-direction: column;
+  &:after {
+    content: '';
+    margin-top: 5px;
+    display: block;
+    height: 1px;
+    background: ${COLOR_CLAVE_PINK};
+  }
+  &:hover:after {
+    content: '';
+    margin-top: 5px;
+    display: block;
+    height: 1px;
+    animation: ${hoverAnimation};
+    animation-fill-mode: forwards;
+  }
+
+  ${onMobile(`
+    display: none;
+  `)}
+`;
 
 const ANCHOR_STYLES = `
   ${onMobile(`
     display: none;
   `)}
   text-decoration: none;
-  padding-bottom: 5px;
-  border-bottom: 1px solid ${COLOR_CLAVE_PINK};
 `;
 
 const StyledAnchor = styled.a`
   ${ANCHOR_STYLES}
 `;
-
 const StyledLink = styled(Link)`
   ${ANCHOR_STYLES}
 `;
