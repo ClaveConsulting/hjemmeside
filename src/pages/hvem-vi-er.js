6ed58fa1adcf8a15f5b43213ca28831721db5266
Mathias Aarseth Pedersen <Mathiasaarseth.pedersen@naf.no>
2020-06-23 17:48:46 +0200

    Oppdaterte tekster

--- src/pages/hvem-vi-er.js
+++ src/pages/hvem-vi-er.js
@@ -42,24 +42,32 @@ const HvemViEr = props => {
             Vi mener at det er godt og litt annerledes å være en Claver. Vi
             føler en tilhørighet både til selskapet og til kollegaene våre.
           </h2>
+
           <p>
             Det er flest hverdager i året, og i denne hverdagen setter vi
-            trivsel og trygghet høyt. Når vi er trygge på hverandre får vi god
-            lagånd og det er da vi kan lage fantastiske løsninger.
+            trivsel og trygghet høyt. Vi har det morsomt på jobb. Tverrfaglig
+            samarbeid gjør oss sterke i møtet med faglige utfordringer. Lagånd,
+            positivitet og omsorg for kollegaene våre gjør oss trygge på
+            hverandre. Da kan vi lage de aller beste løsningene.
           </p>
           <p>
-            Det er smart å ta avgjørelser sammen. Åpenheten vi har gjør at vi
-            vet hva som rører seg i selskapet, og da er det også enklere å
-            påvirke retning. Siden selskapet er eid av oss ansatte er det også
-            vi som bestemmer.
+            Det er smart å ta avgjørelser sammen. Åpenhet gjør at vi vet hva som
+            rører seg i selskapet, og da er det enkelt å påvirke hvilken retning
+            vi skal gå. Clave er eid av oss ansatte, og derfor er det også vi
+            som bestemmer.
           </p>
 
           <p>
             Sammen reiser vi ut i verden på jakt etter inspirasjon. Faglig
-            utvikling er viktig for oss, både som team og for hver enkelt. Vi
-            har innedager vi fyller med faglig innhold og prosjekter. Sammen ser
-            vi muligheter, og vi skaper et kreativt fellesskap. Teknologi og
-            brukeropplevelse er det beste vi vet.
+            utvikling er viktig for oss - både som team og for hver enkelt. Vi
+            har innedager vi fyller med prosjekter og hobbyer. Sammen skaper vi
+            et kreativt felleskap hvor vi får mulighet til å ta i bruk mange -
+            og kanskje nye sider - av oss selv.
+          </p>
+          <p>
+            Teknologi og brukeropplevelse er det beste vi vet. Når vi lærer nye
+            ting, inspireres av hverandre og føler engasjement skaper vi verdi
+            både for kundene våre og oss selv.
           </p>
         </Layout>
       </main>
