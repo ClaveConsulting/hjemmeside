cc0de8dc4e524e26733dedc52b75a801bd4244c3
tmordal <95020266+tmordal@users.noreply.github.com>
2021-11-25 10:41:13 +0100

    Tina: Oppdatering av resten av tekst
    Oppdatering av hele teksten fra Leni

--- src/pages/jobb-som-designer.js
+++ src/pages/jobb-som-designer.js
@@ -38,29 +38,47 @@ const DeveloperPage = props => {
       <Layout>
         <h1>Jobbe som designer</h1>
         <Ingress>
-        Vi omskaper gode ideer til fungerende løsninger ved hjelp av dialog, faglig tyngde og grundige designprosesser. Gjennom å lage intuitive og tiltalende designløsninger forener vi brukernes behov med kundenes ønsker. Det er viktig for Clave at vi leverer nøkkelpersoner med stort hjerte for faget og kundenes virksomhet.
+        Vi omskaper gode ideer til fungerende løsninger ved hjelp av dialog, faglig tyngde og grundige designprosesser. 
+        Gjennom å lage intuitive og tiltalende designløsninger forener vi brukernes behov med kundenes ønsker. 
+        Det er viktig for Clave at vi leverer nøkkelpersoner med stort hjerte for faget og kundenes virksomhet.
         </Ingress>
       </Layout>
       <MidSplitLayout image={images.pia}>
-        <h2>Kan dette være deg?</h2>
+        <h2>Er dette deg?</h2>
         <p>
-          Vi i Clave liker folk som legger sjelen sin i å utforske, analysere og forstå. Og ikke er fremmed for å hoppe ut i ukjent farvann med hodet først. Å visualisere nye konsepter er ingen sak for deg, og du presenterer gjerne for kunden med stor entusiasme.
+          Vi i Clave liker folk som legger sjelen sin i å utforske, analysere og forstå, og som ikke er fremmed for å hoppe ut i ukjent farvann. 
+        Å visualisere nye konsepter er ingen sak for deg, og du finner stor glede i å videreutvikle disse konseptene sammen med utviklere og kunder.
         </p>
         <p>
-          Vi ser etter deg som kan identifisere og analysere brukerbehov og krav. 
-          Med høy entusiasme utvikler og visualiserer konsepter. 
-          Er god på å planlegge, gjennomføre og presentere brukertester. 
-          Evaluere utviklet funksjonalitet og sørge for at det er kvalitet våre leveranser
+        Vi ser etter deg som kan identifisere og analysere brukerbehov og krav. Du er god på å planlegge, gjennomføre og presentere brukerinnsikt. 
+        Samtidig er du i stand til å evaluere utviklet funksjonalitet og sørge for at leveransene våre har god kvalitet.
         </p>
         <p>
           Det skader ikke om du også:
           <List>
-            <li>brenner for å videreutvikle deg</li>
+            <li>syns faglig utvikling er viktig</li>
             <li>trives i et sosialt arbeidsmiljø og kommuniserer godt med andre</li>
-            <li>er selvgående og har viljen til å stå opp for det du oppriktig tror er best</li>
+            <li>er selvgående og har evne til å stå for det du mener er den beste løsningen på faglige utfordringer</li>
           </List>
         </p>
       </MidSplitLayout>
+  <h2>Hos oss i Clave</h2>
+        <p>
+        Det er veldig viktig for oss at alle skal oppleve et godt arbeidsmiljø og stor takhøyde. Dessuten har vi det morsomt på jobb! 
+          Gjennom tverrfaglig samarbeid blir vi sterke i møtet med faglige utfordringer, og utvikler oss både som fagpersoner og kollegaer.
+        </p>
+        <p>
+        Lagånd, positivitet og omsorg for kollegaene våre gjør oss trygge på hverandre og på jobben vi gjør. 
+        I Clave tror vi det er oppskriften på å lage de aller beste løsningene.
+        </p>
+        <p>
+          Vi jobber i spennende og varierte prosjekter ute hos kundene våre, og du kan selv påvirke hva du vil jobbe med og i hvilken retning du ønsker å utvikle deg. 
+          Lederskap basert på tillit til de ansatte står sentralt, og det er en selvfølge at du får mulighet til å påvirke din egen arbeidshverdag.
+        </p>
+         <p>
+          Clave tilbyr konkurransedyktige betingelser, og et aksjeprogram med mulighet til å bli medeier. 
+          Og viktigst av alt: Et inkluderende og hyggelig arbeidsmiljø med ydmyke og flinke kolleger.
+        </p>
     </RecruitmentTemplate>
   )
 }
