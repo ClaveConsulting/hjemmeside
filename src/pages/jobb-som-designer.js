f0818112a2956a645a9d2a6509db4b8369de37e3
Morten Tangen <morten.tangen@gmail.com>
2022-10-12 11:14:10 +0200

    legg til 'Deltid- og sommerjobb som UX-designer' i positions (#46)
    Co-authored-by: Morten S. Tangen <morten.tangen@eika.no>

--- src/pages/jobb-som-designer.js
+++ src/pages/jobb-som-designer.js
@@ -9,11 +9,16 @@ import { Ingress } from '../components/Ingress';
 
 const positions = [
   {
-    text: "Sommerjobb hos Clave 2023",
+    text: 'Sommerjobb hos Clave 2023',
     active: false,
-    url: "https://clave.homerun.co/sommerjobb-hos-clave-2023/no"
+    url: 'https://clave.homerun.co/sommerjobb-hos-clave-2023/no',
   },
-]
+  {
+    text: 'Deltid- og sommerjobb som UX-designer',
+    active: true,
+    url: 'https://clave.homerun.co/deltid-og-sommerjobb-som-ux-designer'
+  },
+];
 
 
 const DeveloperPage = props => {
@@ -28,25 +33,30 @@ const DeveloperPage = props => {
   return (
     <RecruitmentTemplate
       positions={positions}
-      title="Jobbe som designer"
+      title='Jobbe som designer'
     >
       <Layout>
         <h1>Jobbe som designer</h1>
         <Ingress>
-        Vi omskaper gode ideer til fungerende løsninger ved hjelp av dialog, faglig tyngde og grundige designprosesser. 
-        Gjennom å lage intuitive og tiltalende designløsninger forener vi brukernes behov med kundenes ønsker. 
-        Det er viktig for Clave at vi leverer nøkkelpersoner med stort hjerte for faget og kundenes virksomhet.
+          Vi omskaper gode ideer til fungerende løsninger ved hjelp av dialog, faglig tyngde og grundige
+          designprosesser.
+          Gjennom å lage intuitive og tiltalende designløsninger forener vi brukernes behov med kundenes ønsker.
+          Det er viktig for Clave at vi leverer nøkkelpersoner med stort hjerte for faget og kundenes virksomhet.
         </Ingress>
       </Layout>
       <MidSplitLayout image={images.pia}>
         <h2>Er dette deg?</h2>
         <p>
-          Vi i Clave liker folk som legger sjelen sin i å utforske, analysere og forstå, og som ikke er fremmed for å hoppe ut i ukjent farvann. 
-        Å visualisere nye konsepter er ingen sak for deg, og du finner stor glede i å videreutvikle disse konseptene sammen med utviklere og kunder.
+          Vi i Clave liker folk som legger sjelen sin i å utforske, analysere og forstå, og som ikke er fremmed for å
+          hoppe ut i ukjent farvann.
+          Å visualisere nye konsepter er ingen sak for deg, og du finner stor glede i å videreutvikle disse konseptene
+          sammen med utviklere og kunder.
         </p>
         <p>
-        Vi ser etter deg som kan identifisere og analysere brukerbehov og krav. Du er god på å planlegge, gjennomføre og presentere brukerinnsikt. 
-        Samtidig er du i stand til å evaluere utviklet funksjonalitet og sørge for at leveransene våre har god kvalitet.
+          Vi ser etter deg som kan identifisere og analysere brukerbehov og krav. Du er god på å planlegge, gjennomføre
+          og presentere brukerinnsikt.
+          Samtidig er du i stand til å evaluere utviklet funksjonalitet og sørge for at leveransene våre har god
+          kvalitet.
         </p>
         <p>
           Det skader ikke om du også:
@@ -58,11 +68,11 @@ const DeveloperPage = props => {
         </p>
       </MidSplitLayout>
     </RecruitmentTemplate>
-  )
-}
+  );
+};
 
 const List = styled.ul`
   margin: 20px 40px;
-`
+`;
 
 export default DeveloperPage;
