df64be513ddcb0d64a1066b7ec4c0042ed6ada2d
Yngve Svalestuen <yngve.svalestuen@clave.no>
2022-11-18 10:05:15 +0100

    We should link to the job e-mail in the ContactInfo box

--- src/components/Ingress.js
+++ src/components/Ingress.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
-import { onMobile } from "./Breakpoints";
+import { onMobile } from './Breakpoints';
 
-export const Ingress = styled.p`  
-  font-size: 2.5rem;
-  line-height: 3rem;
-  font-weight: 400;
-  ${onMobile(`    
-    font-size: 1.5rem;
-    line-height: 2rem;
+export const Ingress = styled.p`
+  &,
+  & span,
+  & a {
+    font-size: 2.5rem;
+    line-height: 3rem;
+    font-weight: 400;
+    ${onMobile(`    
+      font-size: 1.5rem;
+      line-height: 2rem;
   `)}
-`
+  }
+`;
--- src/components/RecruitmentPages/ContactInfo.js
+++ src/components/RecruitmentPages/ContactInfo.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Layout from "../Layout";
+import Layout from '../Layout';
 import { Ingress } from '../Ingress';
+import styled from 'styled-components';
+
+const Phone = styled.span`
+  white-space: nowrap;
+`;
 
 const ContactInfo = () => {
   return (
     <Layout>
       <Ingress>
-        Ønsker du å stille oss flere spørsmål? <br/> Ring Terje på +47 975 37 841 eller send oss en mail
+        Ønsker du å stille oss flere spørsmål? <br /> Ring Terje på{' '}
+        <Phone>+47 975 37 841</Phone> eller send oss en{' '}
+        <a href="mailto:jobb@clave.no">mail</a>.
       </Ingress>
     </Layout>
-  )
-}
+  );
+};
 
 export default ContactInfo;
