d15581f7d77e4d38ec1a0e153390ed9667f7c21a
Eirik Vilpponen <eirik.vilpponen@clave.no>
2024-01-03 16:03:40 +0100

    rename files in clave old from .js to .jsx

---
+++ src/components/clave-old/components/Title.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { COLOR_CLAVE_GREEN } from '../colors';
+
+
+const StyledWrapper = styled.h1`
+  color: ${props => props.color};
+  @media only screen and (min-width: 600px) {
+  font-size:3rem;
+  }
+`;
+const Title = ({ children, color = COLOR_CLAVE_GREEN, className }) => (
+  <StyledWrapper className={className} color={color}>{children}</StyledWrapper>
+);
+
+Title.propTypes = {
+  children: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+export default Title;
---
+++ src/components/clave-old/pages/prosjekter/the-well.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+  ProjectPageTemplate,
+} from '../../components/ProjectTemplate';
+import { useStaticQuery, graphql } from 'gatsby';
+import { COLOR_CLAVE_GREEN, COLOR_CLAVE_SUNNY } from '../../colors';
+
+const ProjectPage = () => {
+  const images = useStaticQuery(graphql`
+    query {
+      main: file(relativePath: { eq: "190920_Clave_lowres_15.jpg" }) {
+        ...fluidImage
+      }
+      left: file(relativePath: { eq: "190920_Clave_lowres_14.jpg" }) {
+        ...fluidImage
+      }
+      right: file(relativePath: { eq: "190920_Clave_lowres_12.jpg" }) {
+        ...fluidImage
+      }
+    }
+  `);
+
+  return (
+        <ProjectPageTemplate
+          mainImg={images.main}
+          firstImg={images.left}
+          secondImg={images.right}
+          title="The Well"
+          ingress="Hos The Well jobber vi for å gjøre det enkelt å slappe av. Vi har fått muligheten til å jobbe med nytt design på nettsidene, kjøpsprosessen og ny bookingløsning."
+          keywords={[
+            'Oppstart 2019',
+            'Omtrent 5.000 arbeidstimer per år',
+            'UX’er, front- og backend utviklere',
+          ]}
+          leftImg={images.first}
+          rightImg={images.second}
+          methodology="Lean"
+          technology="Teknologi .NET Core, React.js, Azure, MS SQL Server, Azure DevOps, Umbraco CMS, Kanban"
+          finalText="The Well er Nordens største spa- og velværesenter som ligger på Kolbotn. Her finner du 10,500 kvm med bassenger og forskjellige saunarom."
+          backgroundColor={COLOR_CLAVE_SUNNY}
+          textColor={COLOR_CLAVE_GREEN}
+        >
+          <div>
+            <p>
+              Gjennom månedlige brukertester har vi lært oss å kjenne brukerne
+              til The Well, og en smidig utvilingsprosess gjør at vi raskt kan
+              endre de tingene som ikke fungerer til en lav kostnad. Vårt
+              samarbeid med The Well startet med at vi lagde en ny
+              bookingløsning som gjør det enkelt å bestille spainngang,
+              behandlinger, bussbilletter og å reservere bord i restauranten.
+              Løsningen fungerer veldig bra fordi den er skreddersydd til
+              behovene til The Well, og omsetningen på nett har aldri vært så
+              høy som nå.
+            </p>
+            <p>
+              Vi har også satt sammen et splitter nytt design både på
+              nettsidene (thewell.no) og videreutviklet bookingsløsningen i
+              forbindelse med at The Well åpner et nytt hotell sommeren 2021.
+              Her har vi jobbet i alle stadier av prosjektet - fra
+              konseptutvikling til ferdig produkt. I 2020 har vi også utviklet
+              en intern løsning for å gjøre det enkelt for de ansatte i The
+              Well å håndtere alle bookinger.
+            </p>
+            <p>
+              I løpet av prosjektet har vi vært alt fra 3 til 6 personer:
+              Prosjektleder, UX-designer og utvikler fra Clave, samt utviklere
+              fra Fabres i Polen.
+            </p>
+          </div>
+        </ProjectPageTemplate>
+  );
+};
+
+export default ProjectPage;
